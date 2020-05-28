032d83379edbf63972a22d59613759e93fbf0100
ecklf <ecklf@icloud.com>
2020-05-28 07:19:43 +0200

    feat: add new icons

---
+++ src/icons/ArrowsExpandOutline.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Svg, { Path, SvgProps } from "react-native-svg";
+interface Props extends SvgProps {
+  size?: number;
+}
+
+const ArrowsExpandOutline = ({ size = 24, ...props }: Props) => {
+  return (
+    <Svg
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      width={size}
+      height={size}
+      {...props}
+    >
+      <Path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"
+      />
+    </Svg>
+  );
+};
+
+export default ArrowsExpandOutline;
---
+++ src/icons/FolderRemove.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Svg, { Path, SvgProps } from "react-native-svg";
+interface Props extends SvgProps {
+  size?: number;
+}
+
+const FolderRemove = ({ size = 24, ...props }: Props) => {
+  return (
+    <Svg
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      width={size}
+      height={size}
+      {...props}
+    >
+      <Path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
+      <Path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M8 11h4"
+      />
+    </Svg>
+  );
+};
+
+export default FolderRemove;
