31ae4920d306eed8d8734c568d8640a3b9df5fd8
ecklf <ecklf@icloud.com>
2021-05-27 13:03:49 +0200

    2.0.0

--- compile.js
+++ compile.js
@@ -1,34 +1,43 @@
 const { build } = require("esbuild");
 const glob = require("tiny-glob");
+const fs = require("fs/promises");
 
 (async () => {
+  // solid
   const solidEntryPoints = await glob("./src/solid/*.{ts,tsx}");
+
   await build({
     entryPoints: solidEntryPoints,
     format: "cjs",
     minify: true,
-    outdir: "./dist/solid",
+    outdir: "./solid",
   });
 
   await build({
     entryPoints: solidEntryPoints,
     format: "esm",
     minify: true,
-    outdir: "./dist/solid/esm",
+    outdir: "./solid/esm",
   });
 
+  await fs.writeFile("./solid/package.json", `{"module": "./esm/index.js"}`);
+
+  // outline
   const outlineEntryPoints = await glob("./src/outline/*.{ts,tsx}");
+
   await build({
     entryPoints: outlineEntryPoints,
     format: "cjs",
     minify: true,
-    outdir: "./dist/outline",
+    outdir: "./outline",
   });
 
   await build({
     entryPoints: outlineEntryPoints,
     format: "esm",
     minify: true,
-    outdir: "./dist/outline/esm",
+    outdir: "./outline/esm",
   });
+
+  await fs.writeFile("./outline/package.json", `{"module": "./esm/index.js"}`);
 })();
