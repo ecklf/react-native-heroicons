f5ed5ae0838af2b96c5a61cf7477143051b8b0cb
ecklf <ecklf@icloud.com>
2021-07-12 10:07:21 +0200

    feat: use correct plus icon

--- src/solid/PlusIcon.tsx
+++ src/solid/PlusIcon.tsx
@@ -15,7 +15,7 @@ const PlusIcon = ({ size = 24, ...props }: Props) => {
     >
       <Path
         fillRule="evenodd"
-        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
         clipRule="evenodd"
       />
     </Svg>
